3eb0322142c0d192c5a70a5d3774e7a03b33a041
PallabKumarS <pallabkumar26@gmail.com>
2025-05-12 19:28:05 +0600

    fix: handle undefined path in Zod error handling
    Ensure the 'path' property in Zod error handling defaults to 'unknown' when the path is undefined, preventing potential runtime errors.

--- src/app/errors/handleZodError.ts
+++ src/app/errors/handleZodError.ts
@@ -4,7 +4,7 @@ import { TErrorSources, TGenericErrorResponse } from './error.interface';
 const handleZodError = (err: ZodError): TGenericErrorResponse => {
   const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
     return {
-      path: issue?.path[issue.path.length - 1],
+      path: issue?.path[issue.path.length - 1] || 'unknown',
       message: issue.message,
     };
   });
